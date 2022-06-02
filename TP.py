e0dc0f3727b262ba0ccba9a9b5a090be0b8d7037
Noceloni <julian_noceloni@hotmail.com>
2022-06-02 14:24:17 -0300

    Arreglo del codigo

--- TP.py
+++ TP.py
@@ -1,39 +1,43 @@
-import pybrain as pb
-import numpy as np
 import pandas as pd
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.structure import TanhLayer
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
 
-#Importamos el excel con datos para el input
+#Importamos los excel con datos para el input
 basedatos = pd.read_excel(r'./Historico.xlsx', sheet_name='Table 1')
 columnas = basedatos.columns
 columnas = columnas.tolist()
 valores = basedatos.values
 
+bd = pd.read_excel(r'./Validacion.xlsx', sheet_name='Table 1')
+columns = bd.columns
+columns = columns.tolist()
+values = bd.values
+
 network = buildNetwork(6, 4, 1, bias=True, hiddenclass=TanhLayer)
 
 # Creamos un dataset que matchee con los tamaños de entrada y salida de la red
-nand_gate = SupervisedDataSet(6, 1)
+entrenamiento = SupervisedDataSet(6, 1)
   
 # Creamos un dataset para testing
-nand_train = SupervisedDataSet(6, 1)
+validacion = SupervisedDataSet(6, 1)
 
 for i in valores:
 
-    # Ajustamos el input y los objetivos a los parámetros del dataset para la tabla de verdad nand_gain
-    nand_gate.addSample((i[1], i[2], i[3], i[4], i[5], i[6]), (i[7],))
- 
-for i in valores:
+    # Ajustamos el input y los objetivos a los parámetros del dataset para el entrenamiento de la red
+  entrenamiento.addSample((i[1], i[2], i[3], i[4], i[6], i[7]), (i[8],))
 
-  # Ajustamos el input y los objetivos a los parámetros del dataset para la tabla de verdad nand_train
-  nand_train.addSample((i[1], i[2], i[3], i[4], i[5], i[6]), (i[7],))
+for i in values:
 
-# Entrenamos la red con el dataset nand_gate
-trainer = BackpropTrainer(network, nand_gate)
-  
-# Iteramos 100 veces para entrenar la red
-for epoch in range(5):
-    trainer.train()
-    trainer.testOnData(dataset=nand_train, verbose=True)
+    # Ajustamos el input y los objetivos a los parámetros del dataset para la validacion de los datos de entrenamiento de la red
+  validacion.addSample((i[1], i[2], i[3], i[4], i[6], i[7]), (i[8],))
+
+# Entrenamos la red con el dataset entrenamiento
+trainer = BackpropTrainer(network, dataset=entrenamiento)
+
+# Iteramos 10 veces para entrenar la red
+for epoch in range(10):
+  trainer.train()
+
+trainer.testOnData(dataset=validacion, verbose=True)
